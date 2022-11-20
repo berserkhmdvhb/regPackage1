772a267affeca0f7cbef690d561f2259c0836ae9
Hamed <hamed.vaheb.001@student.uni.lu>
2022-11-20 18:43:05 +0100

    correcting prepare_hmd for discrete case.

--- R/prepare_hmd.R
+++ R/prepare_hmd.R
@@ -22,30 +22,30 @@ prepare_hmd <- function(data=insurance_data,
   df <- data.frame({{data}})
   # clean names of columns of dataframe
   df <- janitor::clean_names(df)
-  cat_cols <- janitor::make_clean_names(cat_cols)
+  cat_cols_cleaned <- janitor::make_clean_names({{cat_cols}})
   # replace "Inf" and na values with NA
   # this is needed when appplying
   df[is.na(df) | df=="Inf"] = NA
 
 
-  if (!(typeof(cat_cols) %in% c("list","character")))
+  if (!(typeof(cat_cols_cleaned) %in% c("list","character")))
   {
     warning("Please input cat_cols as either character type, e.g. c(col1,col2,...) or list type list(col1,col2,...)")
   }
   else
   {
-    if (length(cat_cols) == 0)
+    if (length(cat_cols_cleaned) == 0)
     {
       return(df)
     }
     else
     {
 
-      for (col in cat_cols)
+      for (col in cat_cols_cleaned)
       {
           if (!(col %in% names(df)))
           {
-            warning("A categorical column from your input list cat_cols is not among dataframe's columns.")
+            warning("A categorical column from your input list cat_cols is not among dataframe columns.")
             break
           }
           df[col] = as.factor(df[col])

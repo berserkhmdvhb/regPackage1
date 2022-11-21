90ae6caac529be63dd5ce637eaa26be1626430ba
Hamed <hamed.vaheb.001@student.uni.lu>
2022-11-21 08:56:10 +0100

    adding glmnet

--- R/glmnet_hmd.R
+++ R/glmnet_hmd.R
@@ -1,4 +1,4 @@
-#' GLM Model
+#' GLMNET model
 #' @param data An arbitrary dataframe
 #' @param features_names Name of features. If not specified, takes all columns
 #' except the target
@@ -11,10 +11,10 @@
 #' dataframe by specifying feature names (response variables),
 #' target variable, family of distribution, and the dataset (in my case, Medical Cost Personal Datasets)
 
-glm_hmd <- function(data=insurance_data,
-                      features_names=names({{data}})[names({{data}}) != {{target}}],
-                      target="charges",
-                      family="gaussian"){
+glmnet_hmd <- function(data=insurance_data,
+                    features_names=names({{data}})[names({{data}}) != {{target}}],
+                    target="charges",
+                    family="gaussian"){
   # ensure dataframe is not empty
   features_names_main <- {{features_names}}
   if(nrow({{data}}) == 0) {
@@ -30,11 +30,11 @@ glm_hmd <- function(data=insurance_data,
     features_names_main <- names(df)[names(df) != {{target}}]
   }
   else{
-  if (length(features_names) == 0)
-  {
-    warning("List is empty. All columns except target will be selected")
-    features_names_main <- names(df)[names(df) != {{target}}]
-  }
+    if (length(features_names) == 0)
+    {
+      warning("List is empty. All columns except target will be selected")
+      features_names_main <- names(df)[names(df) != {{target}}]
+    }
     else
     {
       if (!(all(features_names_main %in% names(df))))
@@ -46,15 +46,12 @@ glm_hmd <- function(data=insurance_data,
     }
   }
 
-
-  glm_format <- stats::as.formula(paste({{target}}, "~",
-                   paste(features_names_main, collapse = "+"),
-                   sep = ""
-                  ))
-  fit <- stats::glm(glm_format,
-             data=df,
-             family={{family}}
-             )
+  features <- as.numeric(df[features_names_main])
+  target_col <- as.numeric(df[{{target}}])
+  fit <- glmnet::glmnet(features,
+                        target_col,
+                        family={{family}}
+  )
   return(fit)
 }
 

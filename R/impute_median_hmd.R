e65991a7753f3d0e31c7a3c59fee490a21dae520
Hamed <hamed.vaheb.001@student.uni.lu>
2022-12-26 18:44:35 +0100

    automated detection of columnts intended for imputation

--- R/impute_median_hmd.R
+++ R/impute_median_hmd.R
@@ -1,54 +1,25 @@
 #' Imputes missing values with median
 #' @param data An arbitrary dataframe
-#' @param cols_list List of numerical columns intended to be imputed
 #' @export
 #' @return Returns dataframe with its categorical columns factorised
 #' @details
-#' This function receives a dataframe and a list of columns to be imputed with median.
-#' Then it replaces NA values of each column from the given list with median of that column.
+#' This function receives a dataframe, finds columns containing NA values, and then
+#' replaces NA values of each founded column with median of that column.
 
 
-impute_median_hmd <- function(data=regPackage1::car_insurance_data,
-                              cols_list =list()){
+impute_median_hmd <- function(data=regPackage1::car_insurance_data){
   # ensure dataframe is not empy
   if(nrow({{data}}) == 0) {
     warning("The returned data frame is empty.")
   }
-
-
-  #ensure user inputs a proper object for cat_cols
-  if (!(typeof({{cols_list}}) %in% c("list","character")))
+  cols_list = which(colSums(is.na(df))>0)
+  if (length({{cols_list}}) == 0)
   {
-    warning("Please input cat_cols as either character type, e.g. c(col1,col2,...) or list type list(col1,col2,...)")
+    warning("No column has NA value. No columns will be modified.")
+    return(df)
   }
-  else
-  {
-    #ensure use doesn't input an empty list for cat_cols
-    if (length({{cols_list}}) == 0)
-    {
-      warning("No column is selected. No columns will be modified.")
-      return(df)
-    }
-    else
-    {
-      df <- data.frame({{data}})
-      if (!(all({{cols_list}} %in% names(df))))
-      {
-        warning("A categorical column from your input list cat_cols is not among dataframe columns.")
-      }
-      else{
-        for (col in {{cols_list}}){
-          if (!(is.numeric(df[[col]])))
-          {
-            warning("A categorical column from your input list cat_cols is not among dataframe columns.")
-            break
-          }
-          df[[col]][is.na(df[[col]])] <- stats::median(df[[col]], na.rm = T)
-
-        }
-      }
-
-    }
+  for (col in {{cols_list}}){
+    df[, col][is.na(df[, col])] <- stats::median(df[, col], na.rm = T)
   }
   return(df)
 }
--- R/preprocess_hmd.R
+++ R/preprocess_hmd.R
@@ -1,12 +1,12 @@
-#' Prepare dataframe to bed fed into models
+#' Prepare dataframe to be fed into models
 #' @param data An arbitrary dataframe
 #' @export
 #' @return Returns cleaned dataframe ready to be fed into models
 #' @details
-#' This functions allows to clean and prepare dataframe by cleaning column names, automatically detecting the
-#' categorical columns and then factorizing them, and replace "na" and "Inf" values with NA
-
-
+#' This function is a wrapper around all functions defined in the preprocessing stage.
+#' It allows the user to use the raw original data and using this function receive a data
+#' ready to be fed into models. This function applies the following functions on the given dataframe:
+#' janitor::clean_names, regPackage1::categoricals_hmd, regPackage1::impute_median_hmd
 
 preprocess <- function(data=regPackage1::car_insurance_data){
   # ensure dataframe is not empy
@@ -17,9 +17,7 @@ preprocess <- function(data=regPackage1::car_insurance_data){
   df <- data.frame({{data}})
   # clean names of columns of dataframe
   df <- janitor::clean_names(df)
-
-  # replace "Inf" and na values with NA
-  # this is needed when applying models
-  df[is.na(df) | df=="Inf"] = NA
+  df <- categoricals_hmd(df)
+  df <- impute_median_hmd(df)
   return(df)
 }

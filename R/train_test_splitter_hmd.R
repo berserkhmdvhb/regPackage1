63bdce0bd9279e632e3da68f27079f8a9c0f674e
Hamed <hamed.vaheb.001@student.uni.lu>
2022-12-26 21:53:53 +0100

    modified train test splitter

---
+++ R/train_test_splitter_hmd.R
@@ -0,0 +1,25 @@
+#' Splitting and Normalizind Data
+#' @param data An arbitrary dataframe
+#' @param proportion Proportion of train to test. Default is 0.7, which results
+#' @export
+#' @return Returns a dictionary containing train and test data
+#' @details
+#' This functions allows the user to split data into train and test
+
+
+
+splitter_norm_hmd <- function(data=regPackage1::car_insurance_data,
+                              proportion=0.7
+                              ){
+  # ensure dataframe is not empty
+  if(nrow({{data}}) == 0) {
+    warning("The returned data frame is empty.")
+  }
+  # make a copy of data with different pointer in memory
+  df <- data.frame({{data}})
+  train <- df |> dplyr::sample_frac({{proportion}})
+  test  <- dplyr::anti_join(df, train, by = 'id')
+  l <- c("train"=train, "test" =test)
+  return(l)
+}
+

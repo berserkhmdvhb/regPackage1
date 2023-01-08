29d16bb5872ed6e74f84f38a5342cb875ea574ee
Hamed <hamed.vaheb.001@student.uni.lu>
2023-01-08 16:46:39 +0100

    attempting to fix server.R

--- my_app/server.R
+++ my_app/server.R
@@ -1,23 +1,35 @@
 server <- function(session, input, output) {
 
-  roc <- reactive(
-      model <- input$model_selected
-      actual <- insurance_test$outcome
-      if (model == "Logistic Regression"){
-        model_glm <- glmnet_fit_hmd(insurance_train,
-                       target = "outcome",
-                       family = "binomial")
-        predictions_glm <- glmnet_predict_hmd(model_glm,
-                           data = insurance_test,
-                           target = "outcome",
-                           type = "response")
-        pred_proba_glmnet <- predictions_glm$predict_proba
-        roc_obj_glmnet <- pROC::roc(actual ~ pred_proba, print.auc=FALSE)
-      }
-
+  roc_obj <- reactive(
+    actual <- insurance_test$outcome
+    if (input$model_selected == "Logistic Regression"){
+      model_glm <- glmnet_fit_hmd(insurance_train,
+                                  target = "outcome",
+                                  family = "binomial")
+      predictions_glm <- glmnet_predict_hmd(model_glm,
+                                            data = insurance_test,
+                                            target = "outcome",
+                                            type = "response")
+      pred_proba_glmnet <- predictions_glm$predict_proba
+      roc_obj_glmnet <- pROC::roc(actual ~ pred_proba_glmnet,
+                                  print.auc=FALSE)
+      roc_obj_glmnet
+    }
+    else if (input$model_selected == "Random Forest"){
+      model_random_forest <- rf_fit_hmd(insurance_train,
+                                        ntree = 300,
+                                        mtry = 10,
+                                        proximity = TRUE,
+                                        importance = FALSE)
+      predictions_rf <- rf_predict_hmd(data=insurance_test,
+                                       fit=model_random_forest)
+      pred_proba_rf <- predictions_rf
+      roc_obj_glmnet <- pROC::roc(actual ~ pred_proba_rf,
+                                  print.auc=FALSE)
+    }
   )
 
   output$roc_curve <- renderPlot({
-    pROC::ggroc(roc_obj)(roc())
+    pROC::ggroc(roc_obj())
   })
 }
--- my_app/ui.R
+++ my_app/ui.R
@@ -1,3 +1,4 @@
+library(shiny)
 ui <- function(request){
   fluidPage(
 

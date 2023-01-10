673be58bc048a8449980d86a3550d6b83c5feadc
Hamed <hamed.vaheb.001@student.uni.lu>
2023-01-10 19:00:58 +0100

    created conditional reactive expressions

--- my_app/server.R
+++ my_app/server.R
@@ -6,63 +6,52 @@ server <- function(session, input, output) {
 
 
 
-  observe({
-    if (model_selected_reac() == "Logistic Regression"){
+  preds <- reactive({
+    if (input$model_selected == "Logistic Regression"){
+      actual <- insurance_test$outcome
+      model_glm <- glmnet_fit_hmd(insurance_train,
+                                  target = "outcome",
+                                  family = "binomial")
+      glmnet_predict_hmd(model_glm,
+                         data = insurance_test,
+                         target = "outcome",
+                         type = "response")
+    }
+    else if (input$model_selected == "Random Forest"){
+      model_random_forest <- rf_fit_hmd(insurance_train,
+                                        ntree = 300,
+                                        mtry = 10,
+                                        proximity = TRUE,
+                                        importance = FALSE)
 
-        output$evaluation_plots <- renderPlot({
-        actual <- insurance_test$outcome
-        model_glm <- glmnet_fit_hmd(insurance_train,
-                                    target = "outcome",
-                                    family = "binomial")
-        predictions_glm <- glmnet_predict_hmd(model_glm,
-                                       data = insurance_test,
-                                       target = "outcome",
-                                       type = "response")
-        if (evaluation_selected_reac() == "Confusion Matrix")
-        {
-          eval_glm <- eval_hmd(insurance_test$outcome,
-                               predictions_glm$predictions)
-          eval_glm$confusion_matrix_plot
-        }
-        else if (evaluation_selected_reac() == "ROC Curve"){
-          pred_proba_glmnet <- get_pred_proba(predictions_glm)
-          roc_obj_glmnet <- roc_obj_cal(actual, pred_proba_glmnet)
-          plot_roc_curve(roc_obj_glmnet)
-        }
+      rf_predict_hmd(data=insurance_test,
+                     fit=model_random_forest)
+    }
+  })
 
-
-        })
-
-      }
-    else if (model_selected_reac() == "Random Forest"){
-
-      output$evaluation_plots <- renderPlot({
-        model_random_forest <- rf_fit_hmd(insurance_train,
-                                          ntree = 300,
-                                          mtry = 10,
-                                          proximity = TRUE,
-                                          importance = FALSE)
-
-        predictions_rf <- rf_predict_hmd(data=insurance_test,
-                                         fit=model_random_forest)
-        if (evaluation_selected_reac() == "Confusion Matrix")
-        {
-          eval_rf <- eval_hmd(insurance_test$outcome,
-                               predictions_rf$predictions)
-          eval_rf$confusion_matrix_plot
-        }
-        else if (evaluation_selected_reac() == "ROC Curve"){
-          pred_proba_rf <- get_pred_proba(predictions_rf)
-          roc_obj_rf <- roc_obj_cal(actual, pred_proba_rf)
-          plot_roc_curve(roc_obj_rf)
-        }
-
-
-      })
-
+  plot_eval <- reactive({
+    actual <- insurance_test$outcome
+    if (input$evaluation_selected == "ROC Curve"){
+      pred_proba <- get_pred_proba(preds())
+      roc_obj <- roc_obj_cal(actual, pred_proba)
+      plot_roc_curve(roc_obj)
+    }
+    else if (input$evaluation_selected == "Confusion Matrix"){
+      eval_glm <- eval_hmd(insurance_test$outcome,
+                           preds()$predictions)
+      eval_glm
+      eval_glm$confusion_matrix_plot
     }
 
-
-
   })
+
+
+  output$evaluation_plots <- renderPlot({
+    plot_eval()
+  })
+
+
+
+
+
 }

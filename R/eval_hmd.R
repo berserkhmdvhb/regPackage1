4e08e8782da6b534f86890522bcfdcb8243611e2
Hamed <hamed.vaheb.001@student.uni.lu>
2023-01-11 16:10:48 +0100

    added type checks to eval_hmd

--- R/eval_hmd.R
+++ R/eval_hmd.R
@@ -1,17 +1,35 @@
-#' Evaluation Metrics Computed
+#' Evaluation Metrics Computation for binary class
 #' @param actual The actual data (usually obtained from the test data)
 #' @param predicted Predictions from an arbitrary model
 #' @export
 #' @return Returns a hash of all evaluation metrics
 #' @details
 #' This functions computes various evaluation metrics of classification for a given
-#' actual data and a prediction data. The function returns a hash containing the
+#' actual data and a prediction data, both of which should be binary class.
+#' The function returns a hash containing the
 #' following items: accuracy, precision, recall, F-score, F-beta score, confusion
 #' matrix, and plot confusion matrix
 eval_hmd <- function(actual,
                     predicted){
   y_actual <- {{actual}}
   y_predicted <- {{predicted}}
+
+  if (is.factor(y_actual)){
+    y_actual <- as.numeric(y_actual)
+  }
+
+  if (is.factor(y_predicted)){
+    y_predicted <- as.numeric(y_predicted)
+  }
+
+  if (!(is.numeric(y_actual)) | !(is.numeric(y_predicted))){
+    stop("The predicted and actual inputs should both contain at most two classes.
+         Please ensure both of their class are either numeric of factor")
+  }
+
+  if (!(actual |> unique() |> length() == 2) | !(actual |> unique() |> length() == 2)){
+    stop("Both actual and predicted should be binary. Ensure they contain only two classes.")
+  }
   cm <- caret::confusionMatrix(data = as.factor(y_predicted),
                                reference = as.factor(y_actual),
                                dnn = c("Prediction", "Reference"))
